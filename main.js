53cf0050fbd55a253fe9b80fc01919ad8ba4443e
LAPT-CSH-12 <cshtd.computacion@gmail.com>
2025-03-04 10:50:33 -0600

    pip

---
+++ main.js
@@ -0,0 +1,32 @@
+//Conexión con HTML (Seleccionar elementos)
+
+const songForm = document.getElementById("song_form");
+const PlayList = document.getElementById("play_list");
+
+// Función para agregar una canción a la lista
+function addSong(songName, artistName, songUrl){
+    const listSong = document.createElement('li');
+    listSong.innerHTML = `
+            <strong> ${songName} </strong>
+            -${artistName}
+            <a href="${songUrl}" target="_blank" >Reproducir</a>
+            <button class="delete-btn" >Eliminar</button>
+    `;
+    //Agregar al HTML
+    PlayList.appendChild(listSong);
+
+}
+//...
+//Manejo de envío de formularios (Eventos)
+
+songForm.addEventListener('submit',(event)=> {
+    event.preventDefault();
+//Obtener datos del formulari
+    const songForm = document.getElementById("song_name").value
+    const artistName = document.getElementById("artist_name").value
+    const songUrl = document.getElementById("url_song").value
+    addSong(songForm, artistName, songUrl);
+
+    songForm.reset()
+
+})
